3a9c04e9630f85868c9d752a68d70ab52b1e4ffc
SebCanet <sebastien.canet@ac-nantes.fr>
2014-12-24 06:37:57 +0100

    v2 !

--- scratch_ex.bat
+++ scratch_ex.bat
@@ -4,4 +4,4 @@ rem fichiers BAT et fork créés par Sébastien CANET
 cls
 SET currentpath=%~dp1
 SET dossier_scratch="Scratch 2.exe"
-start %dossier_scratch% %currentpath%fichiers_vides\axe105.sb2
+start %dossier_scratch% "F:\ZZZzzz\Projets Borland Studio\s2p\Debug_Build\bibliotheque\joystick.sb2"
