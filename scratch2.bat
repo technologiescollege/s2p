22e738e993912f37f1d229e90c448aa594288b2d
SebCanet <sebastien.canet@ac-nantes.fr>
2016-01-03 04:32:27 +0100

    update v0.1.1

--- scratch2.bat
+++ scratch2.bat
@@ -4,4 +4,4 @@ rem fichiers BAT et fork créés par Sébastien CANET
 cls
 SET currentpath=%~dp1
 SET dossier_scratch="Scratch 2.exe"
-start %dossier_scratch% "S:\Perso\GitHub\technologies\s2p\s2p\templates\picaxe-20x2.sb2
+start %dossier_scratch% "F:\ZZZzzz\Projets Borland Studio\s2p\Debug_Build\s2p\templates\picaxe-08m2.sb2
